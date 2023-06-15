2464894d2d40c4840eb9604f0a5be7a04470f566
ChaitanyaKarthikitis <chaitanyakarthikitis@gmail.com>
2023-06-15 12:36:18 +0530

    updated padding bug

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -89,7 +89,7 @@ const Experience = () => {
         }
     ]
   return (
-    <div name='Experience' className='bg-gradient-to-b from-gray-800 to-black w-full max-h-fit pt-8 '>
+    <div name='Experience' className='bg-gradient-to-b from-gray-800 to-black w-full max-h-fit  pt-24 '>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div>
                 <p className='font-bold text-4xl border-b-4 border-gray-500 inline'>Experience</p>
